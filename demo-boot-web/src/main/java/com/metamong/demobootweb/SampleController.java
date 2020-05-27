c7e49825b72bcad2231c09265e89a5b781463724
rim <sandrochia@naver.com>
2020-05-27 18:54:10 +0900

    23강. 핸들러 인터셉터 구현

--- demo-boot-web/src/main/java/com/metamong/demobootweb/SampleController.java
+++ demo-boot-web/src/main/java/com/metamong/demobootweb/SampleController.java
@@ -18,6 +18,16 @@ public class SampleController {
      * @param person
      * @return
      */
+
+    // 핸들러 인터셉터 process (동기적)
+    // preHandle 1
+    // preHandle 2
+    // 요청처리
+    // postHandler 2
+    // postHandler 1
+    // 뷰 렌더링
+    // afterCompletion 2
+    // afterCompletion 1
     @GetMapping(value = "/hello")
     public String hello(@RequestParam("id") Person person) {
 
--- demo-boot-web/src/main/java/com/metamong/demobootweb/WebConfig.java
+++ demo-boot-web/src/main/java/com/metamong/demobootweb/WebConfig.java
@@ -2,6 +2,7 @@ package com.metamong.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,4 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
 //        registry.addFormatter(new PersonFormatter());
 //
 //    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GrettingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi")
+                .order(1);
+    }
 }
