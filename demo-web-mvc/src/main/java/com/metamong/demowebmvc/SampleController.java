bc7e747ffa398919146a2d3bc45588da198e47b0
rim <sandrochia@naver.com>
2020-05-28 16:48:48 +0900

    32강. 요청 맵핑하기 2부 URI 패턴

--- demo-web-mvc/src/main/java/com/metamong/demowebmvc/SampleController.java
+++ demo-web-mvc/src/main/java/com/metamong/demowebmvc/SampleController.java
@@ -1,19 +1,38 @@
 package com.metamong.demowebmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(method = RequestMethod.GET)
+@RequestMapping(value = "/hello")
 public class SampleController {
 
     // method 를 지정하지 않으면, 모든 HTTP method 허용.
 //    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.PUT})
-    @RequestMapping("/hello")
+
+    /**
+     * 요청 식별자 맵핑하기 e.g.
+     * @RequestMapping 는 다음을 지원한다.
+     * ? : 한 글자 ("/hello/???" => "/hello/123")
+     * * : 여러 글자 ("/hello/*" => "/hello/metamong")
+     * **: 여러 패스 ("/hello/** => "/hello/metamong/pokemon")
+     * @param name
+     * @return
+     */
+    @RequestMapping("/{name:[a-z]+}")
     @ResponseBody
-    public String hello() {
-        return "hello";
+    public String hello(@PathVariable String name) {
+        return "hello " + name;
+    }
+
+    // 패턴이 중복되는 경우 더 적합한 핸들러를 호출한다.
+    // URI 확장은 보안 이슈 때문에 사용하지 않는 것을 추천. 특정 컨텐츠 타입을 원한다면 accept header 를 이용하셔라.
+    @RequestMapping({"/metamong", "/metamong/*"})
+    @ResponseBody
+    public String helloMetamong() {
+        return "hello Metamong";
     }
 }
--- demo-web-mvc/src/test/java/com/metamong/demowebmvc/SampleControllerTest.java
+++ demo-web-mvc/src/test/java/com/metamong/demowebmvc/SampleControllerTest.java
@@ -10,8 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -22,17 +21,19 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        this.mockMvc.perform(get("/hello"))
+        this.mockMvc.perform(get("/hello/metamong"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello Metamong"))
+                .andExpect(handler().handlerType(SampleController.class))
+                .andExpect(handler().methodName("helloMetamong"))
+        ;
 
-        this.mockMvc.perform(put("/hello"))
+        this.mockMvc.perform(get("/hello/metamong.xml"))
                 .andDo(print())
-                .andExpect(status().isMethodNotAllowed());
-
-        this.mockMvc.perform(post("/hello"))
-                .andDo(print())
-                .andExpect(status().isMethodNotAllowed());
+                .andExpect(status().isNotFound())
+        ;
 
     }
+
 }
