3d80e60830b3e0357cc04d059aca550920b7b88e
rim <sandrochia@naver.com>
2020-05-28 17:06:50 +0900

    33강. 요청 맵핑하기 3부 미디어 타입

--- demo-web-mvc/src/main/java/com/metamong/demowebmvc/SampleController.java
+++ demo-web-mvc/src/main/java/com/metamong/demowebmvc/SampleController.java
@@ -1,38 +1,20 @@
 package com.metamong.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(value = "/hello")
+@RequestMapping(consumes = MediaType.APPLICATION_XML_VALUE)
 public class SampleController {
 
-    // method 를 지정하지 않으면, 모든 HTTP method 허용.
-//    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.PUT})
+    @GetMapping(
+            value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE) // 이런 경우에는 consumes 가 복수의 타입을 지원 x, 오버라이드 한다.
 
-    /**
-     * 요청 식별자 맵핑하기 e.g.
-     * @RequestMapping 는 다음을 지원한다.
-     * ? : 한 글자 ("/hello/???" => "/hello/123")
-     * * : 여러 글자 ("/hello/*" => "/hello/metamong")
-     * **: 여러 패스 ("/hello/** => "/hello/metamong/pokemon")
-     * @param name
-     * @return
-     */
-    @RequestMapping("/{name:[a-z]+}")
     @ResponseBody
-    public String hello(@PathVariable String name) {
-        return "hello " + name;
-    }
-
-    // 패턴이 중복되는 경우 더 적합한 핸들러를 호출한다.
-    // URI 확장은 보안 이슈 때문에 사용하지 않는 것을 추천. 특정 컨텐츠 타입을 원한다면 accept header 를 이용하셔라.
-    @RequestMapping({"/metamong", "/metamong/*"})
-    @ResponseBody
-    public String helloMetamong() {
-        return "hello Metamong";
+    public String hello() {
+        return "hello";
     }
 }
--- demo-web-mvc/src/test/java/com/metamong/demowebmvc/SampleControllerTest.java
+++ demo-web-mvc/src/test/java/com/metamong/demowebmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,17 +22,12 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        this.mockMvc.perform(get("/hello/metamong"))
+
+        this.mockMvc.perform(get("/hello")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    )
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string("hello Metamong"))
-                .andExpect(handler().handlerType(SampleController.class))
-                .andExpect(handler().methodName("helloMetamong"))
-        ;
-
-        this.mockMvc.perform(get("/hello/metamong.xml"))
-                .andDo(print())
-                .andExpect(status().isNotFound())
         ;
 
     }
